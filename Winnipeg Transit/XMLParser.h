0ff229e8befcc9e77cb6f910acdb94affe8234c7
Marcus Dyck <marcusjbdyck@gmail.com>
2012-03-04 22:48:02 -0600

    Added a class

---
+++ Winnipeg Transit/XMLParser.h
@@ -0,0 +1,13 @@
+//
+//  XMLParser.h
+//  Winnipeg Transit
+//
+//  Created by Marcus Dyck on 12-03-03.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XMLParser : NSObject
+
+@end
---
+++ Winnipeg Transit/XMLParser.m
@@ -0,0 +1,13 @@
+//
+//  XMLParser.m
+//  Winnipeg Transit
+//
+//  Created by Marcus Dyck on 12-03-03.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "XMLParser.h"
+
+@implementation XMLParser
+
+@end
