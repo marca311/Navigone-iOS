962d10f277435d14800263d9fc87927d89ef450a
Marcus Dyck <marcusjbdyck@gmail.com>
2012-03-16 16:38:07 -0500

    Added class

---
+++ Winnipeg Transit/navigoViewLibrary.h
@@ -0,0 +1,19 @@
+//
+//  navigoViewLibrary.h
+//  Winnipeg Transit
+//
+//  Created by Marcus Dyck on 12-03-16.
+//  Copyright (c) 2012 marca311. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface navigoViewLibrary : NSObject
+
+//I have yet to implement either of these
+
+-(void)openTimePicker;
+
+-(void)openDatePicker;
+
+@end
---
+++ Winnipeg Transit/navigoViewLibrary.m
@@ -0,0 +1,13 @@
+//
+//  navigoViewLibrary.m
+//  Winnipeg Transit
+//
+//  Created by Marcus Dyck on 12-03-16.
+//  Copyright (c) 2012 marca311. All rights reserved.
+//
+
+#import "navigoViewLibrary.h"
+
+@implementation navigoViewLibrary
+
+@end
