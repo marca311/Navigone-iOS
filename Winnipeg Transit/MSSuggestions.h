4c4743bd666a3fe8ce5b9b65d08ba932777dba68
Marcus Dyck <marcusjbdyck@gmail.com>
2013-06-11 23:54:25 -0500

    Location suggestion system has been moved over to the new structure.
    Now just the route querying system needs to be done.

---
+++ Winnipeg Transit/MSSuggestions.h
@@ -0,0 +1,13 @@
+//
+//  MSSuggestions.h
+//  Winnipeg Transit
+//
+//  Created by Marcus Dyck on 13-06-11.
+//  Copyright (c) 2013 marca311. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSSuggestions : NSObject
+
+@end
---
+++ Winnipeg Transit/MSSuggestions.m
@@ -0,0 +1,13 @@
+//
+//  MSSuggestions.m
+//  Winnipeg Transit
+//
+//  Created by Marcus Dyck on 13-06-11.
+//  Copyright (c) 2013 marca311. All rights reserved.
+//
+
+#import "MSSuggestions.h"
+
+@implementation MSSuggestions
+
+@end
